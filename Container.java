8faa1f99d604ca96459dcfdddf83a2e176573f39
Arvind741 <ramsooksri@gmail.com>
2026-06-04 11:18:03 -0400

    M1 Completed

--- Container.java
+++ Container.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class Container {
 
     // TODO M1: Initialise this static counter to 1.
-    private static int nextContainerId;
+    private static int nextContainerId= 1;
 
     // TODO M1: These fields are declared but not yet assigned.
     // Your constructors (M2, M3) must assign them.
